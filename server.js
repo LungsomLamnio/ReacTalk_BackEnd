177c4bf65684e0453a9a15c9396bb566c28a9c0d
LungsomLamnio1004 <lungsomlamnio@gmail.com>
2025-06-06 22:55:54 +0530

    database connected successfully

--- server.js
+++ server.js
@@ -1,7 +1,24 @@
 const express = require("express");
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+
 const app = express();
 
+dotenv.config();
+
 const PORT = 3000;
+
+async function main() {
+  await mongoose.connect(process.env.MONGODB_URL);
+}
+
+main()
+  .then(() => {
+    console.log("database connected successfully");
+  })
+  .catch((err) => {
+    console.log(`Error: ${err}`);
+  });
 
 app.get("/", (req, res) => {
   res.send("Home Page");
