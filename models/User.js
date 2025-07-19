c76b9a061c55438718f21c11cdc3b42eb9b5044f
LungsomLamnio1004 <lungsomlamnio@gmail.com>
2025-07-19 14:09:31 +0530

    user model added

---
+++ models/User.js
@@ -0,0 +1,25 @@
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema({
+  username: {
+    type: String,
+    required: [true, "Name is required"],
+    trim: true,
+  },
+  email: {
+    type: String,
+    required: [true, "Email is required"],
+    trim: true,
+    unique: true,
+    lowercase: true,
+  },
+  password: {
+    type: String,
+    required: [true, "Password is required"],
+    minlength: 6,
+  },
+  timestamps: true,
+});
+
+const User = mongoose.model("User", userSchema);
+module.exports = User;
