b095ab4c5c29bb173d373fd2ec1ee5ba070aa1c9
LungsomLamnio1004 <lungsomlamnio@gmail.com>
2025-07-23 13:08:39 +0530

    followers and following added to User Schema

--- models/User.js
+++ models/User.js
@@ -19,6 +19,18 @@ const userSchema = new mongoose.Schema(
       required: [true, "Password is required"],
       minlength: 6,
     },
+    followers: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+      },
+    ],
+    following: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+      },
+    ],
   },
   { timestamps: true }
 );
