b27ad169271a4044abce77741f1188a4cf4fdf23
LungsomLamnio1004 <lungsomlamnio@gmail.com>
2025-08-28 23:37:40 +0530

    update the max file size to 100MB

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -230,7 +230,7 @@ const storage = multer.diskStorage({
 
 const upload = multer({
   storage,
-  limits: { fileSize: 2 * 1024 * 1024 },
+  limits: { fileSize: 100 * 1024 * 1024 },
   fileFilter: function (req, file, cb) {
     if (!file.mimetype.startsWith("image/")) {
       return cb(new Error("Only image files are allowed!"));
